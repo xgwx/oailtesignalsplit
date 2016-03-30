c596a8f5f32094a20fec8fd38e85ac29e5e6d24b
xgwx <chensheng950820@163.com>
2016-03-30 19:57:40 +0800

    Update Lte-softmodem.c
    add oaiudp.c to Lte-softmodem.c file

--- Lte-softmodem.c
+++ Lte-softmodem.c
@@ -129,12 +129,36 @@ unsigned short config_frames[4] = {2,9,11,13};
 #include "stats.h"
 #endif
 
-//udp header
-#ifdef DBS
-#include "oaiudp.h"
-#endif
+//udp varibles
 #ifdef CBS
-#include "oaiudp.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#define OAI_PORT 8888
+#define MAXLINE 1024
+#define DBS_NUM 10
+void *servthread(void *arg);
+int oaisockfd = 0;
+struct sockaddr_in oaiservaddr;
+pthread_t oaiudptid;
+int bsstart = 0;  //state flag to control the start of the basestation;
+struct {
+	int dbsopen;  //dbs state, 0 stands for close, 1 stands for open. only state 1 can provide service;
+	struct sockaddr_in dbssockaddr;
+}dbsstat[DBS_NUM];
+#endif
+#ifdef DBS
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#define OAI_PORT 8888
+#define MAXLINE 1024
+int oaisockfd = 0;
+struct sockaddr_in oaiservaddr;
+pthread_t oaiudptid;
+int bsstart = 0;
+int BSid = -1;
+void *clithread(void *arg);
 #endif
 
 #define FRAME_PERIOD    100000000ULL
@@ -378,6 +402,204 @@ int rrh_UE_port = 51000;
 char uecap_xer[1024],uecap_xer_in=0;
 extern void *UE_thread(void *arg);
 extern void init_UE_threads(void);
+
+#ifdef CBS
+int init_udpserv(void)
+
+{
+
+int temp;
+
+bsstart = 0;
+
+oaisockfd = socket(AF_INET, SOCK_DGRAM, 0); /* create a socket */
+
+/* init servaddr */
+
+bzero(&oaiservaddr, sizeof(oaiservaddr));
+bzero(&dbsstat[0],sizeof(dbsstat[0])*DBS_NUM);
+
+oaiservaddr.sin_family = AF_INET;
+
+oaiservaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+oaiservaddr.sin_port = htons(OAI_PORT);
+
+/* bind address and port to socket */
+
+if(bind(oaisockfd, (strucDBSt sockaddr *)&oaiservaddr, sizeof(oaiservaddr)) == -1)
+
+{
+
+  printf("socket bind error");
+
+  exit(1);
+
+}
+
+if(temp = pthread_create(&oaiudptid,NULL,servthread,NULL)!=0)
+
+{
+
+  printf("can't create thread!");
+
+  exit(1);
+
+}
+
+return 0;
+
+}
+
+void *servthread(void *arg)
+
+{
+
+  char mesg[MAXLINE];
+
+
+  socklen_t len;
+
+  struct sockaddr_in pcliaddr;
+
+  int n;
+  
+//  printf("create server thread...");
+
+  while(1){
+
+  	len = sizeof(pcliaddr);
+
+	n = recvfrom(oaisockfd,mesg,MAXLINE,0,(struct sockaddr*)&pcliaddr,&len);
+
+	if(n == -1){
+
+		printf("read message error!");
+
+		exit(1);
+
+	}
+	mesg[n] = 0;
+
+	//fputs(mesg,stdout);
+   // sendto(oaisockfd,mesg,n,0,(const struct sockaddr*)&pcliaddr,len);
+    if(strcmp(mesg,"DBS connection request")==0){
+	    char sendmsg[] = "0connection request accepted";
+		int tt;
+		for(tt = 0;tt<DBS_NUM;tt++){
+			if(dbsstat[tt].dbsopen == 0){
+				 dbsstat[tt].dbsopen = 1;
+				 memcpy((void *)&dbsstat[tt].dbssockaddr,(void*)&pcliaddr,len);
+				 sendmsg[0]+=tt;
+				 sendto(oaisockfd,sendmsg,sizeof(sendmsg),0,(const struct sockaddr*)&pcliaddr,len);
+				 break;
+			}
+		}
+		bsstart = 1;
+    }
+
+  }
+}
+
+#endif
+
+#ifdef DBS
+int  init_udpcli(char *addrs)
+
+{
+int temp;
+
+bsstart = 0;
+
+oaisockfd = socket(AF_INET, SOCK_DGRAM, 0);
+/* init servaddr */
+
+bzero(&oaiservaddr, sizeof(oaiservaddr));
+
+oaiservaddr.sin_family = AF_INET;
+
+oaiservaddr.sin_port = htons(OAI_PORT);
+
+if(inet_pton(AF_INET, addrs, &oaiservaddr.sin_addr) <= 0)
+{
+
+printf("[%s] is not a valid IPaddress\n", addrs);
+
+exit(1);
+
+}
+
+
+
+
+
+//do_cli(stdin, sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+/* connect to server */
+
+if(connect(oaisockfd, (struct sockaddr *)&oaiservaddr, sizeof(oaiservaddr)) == -1)
+
+{
+
+printf("udp connect error,exit!");
+
+exit(1);
+
+}
+
+if(temp = pthread_create(&oaiudptid,NULL,clithread,NULL)!=0)
+
+{
+
+  printf("can't create thread!");
+
+  exit(1);
+
+}
+
+return 0;
+
+}
+
+
+
+void *clithread(void* arg)
+{
+
+ // char sendline[MAXLINE],recvline[MAXLINE+1];
+  char recvline[MAXLINE];
+  int n;
+
+//  printf("create client thread...");
+
+  while(1){
+        
+        n = recvfrom(oaisockfd,recvline,MAXLINE,0,NULL,NULL);
+
+	if(n == -1){
+
+		printf("read error!");
+
+		exit(1);
+
+	}
+
+	recvline[n] = 0;
+
+	if(n>0&&strcmp(&recvline[1],"connection request accepted")){
+		BSid = recvline[0] - '0';
+		bsstart = 1;
+	}
+	
+	//fputs(recvline,stdout);
+    printf("%s\n",recvline);
+
+
+  }
+
+}
+#endif
+
 
 unsigned int build_rflocal(int txi, int txq, int rxi, int rxq)
 {
@@ -3632,5 +3854,7 @@ void print_opp_meas(void) {
     print_meas(&softmodem_stats_rx_sf[sfn],"[eNB][total_phy_proc_rx]",NULL,NULL);
   }
 }
+
+
 
 
